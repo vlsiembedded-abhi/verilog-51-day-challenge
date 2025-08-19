3bc1b93a68d17c6d0dbc9a419a50629dfa3197d9
Abhishek Dubey <nda681002@gmail.com>
2025-08-19 22:21:31 +0530

    Update tb_2to1mux.v

--- DAY1_MUX2TO1/tb_2to1mux.v
+++ DAY1_MUX2TO1/tb_2to1mux.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.08.2025 16:59:44
-// Design Name: 
-// Module Name: tb_2to1mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 `timescale 1ns/1ps
 module tb_2to1mux;
     reg a,b,sel;
