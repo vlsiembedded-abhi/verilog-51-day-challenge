332fe4a08797a13bcce58f0e2138d2f8d76542ce
Abhishek Dubey <nda681002@gmail.com>
2025-08-19 22:20:46 +0530

    Update mux2to1.v

--- DAY1_MUX2TO1/mux2to1.v
+++ DAY1_MUX2TO1/mux2to1.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 17.08.2025 16:51:29
-// Design Name: 
-// Module Name: mux2to1
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module mux2to1(
     input wire a, // first_input
     input wire b, // second_input
